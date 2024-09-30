5843d8a6045bca14f861173a0bde1fca1f432933
Freddyede <patouillardfranck3@gmail.com>
2024-10-01 00:21:02 +0200

    feature auth readding back nest

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,12 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['http://localhost:5173/', 'http://localhost:5173/*'],
-    },
-  });
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap().then();
+bootstrap();
