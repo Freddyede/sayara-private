9b86d7c642dd58c0ef5f5ac406bfddb2b4f2a685
Freddyede <patouillardfranck3@gmail.com>
2024-09-30 23:46:02 +0200

    feature/initBack
    - app.module.ts: Updated imports
    - main.ts: enable cors with specific origin

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:5173/', 'http://localhost:5173/*'],
+    },
+  });
+  app.enableCors();
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then();
